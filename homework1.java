4d085c7c2778ed3ea093cb894b0631e5fd21cdd3
Anton <kazakov39@gmail.com>
2023-07-10 18:24:10 +0700

    Решил три задачи из первого семинара ДЗ

--- homework1.java
+++ homework1.java
@@ -23,3 +23,100 @@
 // 	}
 // }
 
+// Вывести все простые числа от 1 до 1000
+
+// class homework1 {
+//     public static void main(String[] args)
+//     {
+//         int N = 1000;
+ 
+//         prime_N(N);
+//     }
+//     static void prime_N(int N)
+//     {
+//         int x, y, flg;
+ 
+//         // Printing display message
+//         System.out.println(
+//             "Все простые числа от 1 до  " + N
+//             + ":");
+ 
+//         // Using for loop for traversing all
+//         // the numbers from 1 to N
+//         for (x = 1; x <= N; x++) {
+ 
+//             // Omit 0 and 1 as they are
+//             // neither prime nor composite
+//             if (x == 1 || x == 0)
+//                 continue;
+ 
+//             flg = 1;
+ 
+//             for (y = 2; y <= x / 2; ++y) {
+//                 if (x % y == 0) {
+//                     flg = 0;
+//                     break;
+//                 }
+//             }
+//             if (flg == 1)
+//                 System.out.print(x + " ");
+//         }
+//     }
+// }
+// // Реализовать простой калькулятор
+// import java.util.Scanner;
+
+// class homework1 {
+//   public static void main(String[] args) {
+
+//     char operator;
+//     Double number1, number2, result;
+
+//     // create an object of Scanner class
+//     Scanner input = new Scanner(System.in);
+
+//     // ask users to enter operator
+//     System.out.println("Choose an operator: +, -, *, or /");
+//     operator = input.next().charAt(0);
+
+//     // ask users to enter numbers
+//     System.out.println("Enter first number");
+//     number1 = input.nextDouble();
+
+//     System.out.println("Enter second number");
+//     number2 = input.nextDouble();
+
+//     switch (operator) {
+
+//       // performs addition between numbers
+//       case '+':
+//         result = number1 + number2;
+//         System.out.println(number1 + " + " + number2 + " = " + result);
+//         break;
+
+//       // performs subtraction between numbers
+//       case '-':
+//         result = number1 - number2;
+//         System.out.println(number1 + " - " + number2 + " = " + result);
+//         break;
+
+//       // performs multiplication between numbers
+//       case '*':
+//         result = number1 * number2;
+//         System.out.println(number1 + " * " + number2 + " = " + result);
+//         break;
+
+//       // performs division between numbers
+//       case '/':
+//         result = number1 / number2;
+//         System.out.println(number1 + " / " + number2 + " = " + result);
+//         break;
+
+//       default:
+//         System.out.println("Invalid operator!");
+//         break;
+//     }
+
+//     input.close();
+//   }
+// }
