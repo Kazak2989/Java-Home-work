eaa24d71cd99fc8f3196b4722e2df5afb557578f
Anton <kazakov39@gmail.com>
2023-07-10 16:52:39 +0700

    ДЗ 1 JAva

---
+++ homework1.java
@@ -0,0 +1,25 @@
+// Вычислить n-ое треугольного число (сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
+// Задача на сумму чисел от 1 до n.
+
+// import java.util.Scanner;
+// class homework1{
+//     	public static void main (String[] args)
+// 	{
+// 		Scanner user_input = new Scanner(System.in, "Cp1251");
+//         String n1;
+//         System.out.println("Введите число  n");
+//         n1 = user_input.next();
+//         int n = Integer.parseInt(n1);
+//         int factorial =1;
+//         int sum =0;
+// 		for (int i = 1; i <= n; i++) {
+//             factorial = factorial * i;
+//         }
+// 		for (int j = 0; j <= n; j++) {
+//             sum = sum + j;
+//         }
+//         System.out.printf("the n! of %d is %d%n", n, factorial);
+//         System.out.printf("the sum of %d is %d%n", n, sum);
+// 	}
+// }
+
